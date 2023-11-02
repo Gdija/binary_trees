bfe0e15174ff3b2e227a688933645324979223a6
Gdija <khadijaghadi00@gmail.com>
2023-11-02 08:38:06 -0700

    insert node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_node - create a binary tree node
  * @parent: pointer to the parent node
  * @value: integer to be stored
- * return: pointer to the new node
+ * Return: pointer to the new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
